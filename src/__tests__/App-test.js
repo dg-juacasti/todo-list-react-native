37b0464c1ad22cbdf49b9d8f1339dec3e5a6a90f
Frank Gomez <fdgomez@pichincha.com>
2022-06-24 00:07:41 -0500

    test

--- src/__tests__/App-test.js
+++ src/__tests__/App-test.js
@@ -1,14 +1,49 @@
-/**
- * @format
- */
+import { render, screen, act } from "@testing-library/react-native";
+import axios from "axios";
 
-import 'react-native';
-import React from 'react';
-import App from '../../App';
+describe("TodoList App tests", () => {
+  it("Should render a list with several todos", async () => {
+  });
 
-// Note: test renderer must be required after react-native.
-import renderer from 'react-test-renderer';
+  it("Should create a new todo", async () => {});
 
-it('renders correctly', () => {
-  renderer.create(<App />);
+  /**
+   *
+   * Probar que el formulario muestre los mensajes de requerimiento cuando
+   * el formulario no tenga la descripción y la fecha ingresada
+   */
+  it("Should validate the todo form, description and date required", async () => {});
+
+  it("Should update a todo, description and date", async () => {});
+
+  it("Should delete a todo", async () => {});
+
+  /**
+   *
+   * Probar que el formulario muestre los mensajes de requerimiento cuando
+   * el formulario no tenga la descripción y la fecha ingresada
+   */
+  it("Should update the todo status ", async () => {});
+
+  it("Should show an message when  the todo list is empty  ", async () => {});
+
+  /**
+   *
+   * Probar que la barra de estado cambia cuando se completa una tarea
+   * se puede probar por el cambio en texto o por porcentaje de completitud
+   */
+  it("Should the progress bar change its label text or percentage when a todo is completed ", async () => {});
+
+  /**
+   *
+   * Probar el filtro de las tareas por descripcion
+   */
+  it("Should filter the todo list by description", async () => {});
+
+  /**
+   *
+   * Probar el filtro de tareas que falta por completar y que una vez esten filtradas a dar click nuevamente
+   * sobre el boton del filtro se muestren todos la lista nuevamente
+   */
+  it("Should filter the todo list by completed status and toggle functionality button", async () => {});
 });
