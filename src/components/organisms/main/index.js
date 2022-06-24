3f0e1018f4884b630ca86c612d7145da84926628
Carlos Andres Aguilar Nieto <caaguila@pichincha.com>
2022-06-24 10:31:18 -0500

    empty list

--- src/components/organisms/main/index.js
+++ src/components/organisms/main/index.js
@@ -6,7 +6,8 @@ import {TasksList} from '../../molecules/tasksList';
 import { COLORS } from "../../../shared/colors";
 import fonts from "../../../shared/fonts";
 import {useTasksList} from '../../../hooks/useTasksList';
-
+import Icon from 'react-native-vector-icons/FontAwesome';
+import { View } from "react-native";
 
 export function Main({ navigation }) {
   const {tasks, getTasks} = useTasksList();
@@ -42,9 +43,24 @@ export function Main({ navigation }) {
           iconColor={COLORS.white}
         />
       </StyledSearchContent>
-      <TasksList 
+      {(saveTasks.length === 0) ? (
+        <View>
+          <View style={{alignItems: 'center'}}>
+            <Icon name={'info-circle'} size={30} color={COLORS.textColor} />
+          </View>
+          <Typography
+            caption="No tienes tareas registradas"
+            color={COLORS.textColor}
+            textAlign="center"
+            fontFamily={fonts.RobotoMedium}
+            fontSize={24}
+          />
+        </View>
+      ) : (
+        <TasksList
         tasks={saveTasks}
         navigation={navigation} />
+      )}
     </StyledMain>
   );
 }
--- src/hooks/useTaskCreate.js
+++ src/hooks/useTaskCreate.js
@@ -1,1 +1,20 @@
+import { useState } from 'react'
+import axios from "axios";
+import { BASE_URL, AUTHOR_ID } from '../helpers/constants'
 
+export const useTaskCreate = () => {
+  const [tasks, setTasks] = useState([])
+  const getTasks = () => {
+    axios.post(`${BASE_URL}?id_author=${AUTHOR_ID}`)
+      .then(res => {
+        const { data } = res.data
+        setTasks(data.map(task => ({
+          id: task.id,
+          status: task.status,
+          description: task.description,
+          finish_at: new Date(task.finish_at).toISOString().slice(0, 10),
+      })));
+    })
+  }
+  return { tasks, getTasks}
+}
